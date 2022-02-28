0f7f9aa49b0628c4ef5da81afa309b3834f15ce7
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-28 12:05:12 +0530

    Delete BTN Functionality Done

--- src/app/MyComponents/todo-items/todo-items.component.ts
+++ src/app/MyComponents/todo-items/todo-items.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import {Todo} from 'src/app/Todo';
+
 @Component({
   selector: 'app-todo-items',
   templateUrl: './todo-items.component.html',
@@ -8,9 +9,14 @@ import {Todo} from 'src/app/Todo';
 export class TodoItemsComponent implements OnInit {
   
   @Input() todo:Todo | undefined;
+  @Output() todoDelete:EventEmitter<Todo> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(todo?:Todo){
+    this.todoDelete.emit(todo);
+    console.log("onClick has been Triggered")
+  }
 }
